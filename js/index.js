028b2e8a7096a52980427ee5d75e147be238ffea
Manoel <manoelpiovesan@gmail.com>
2022-07-13 10:06:46 -0300

    inicial

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function f(x){
+    var resultado = parseFloat((Math.pow(x,2) - 2))
+    return resultado
+}
+
+
+document.querySelector('#btn-bissecao').addEventListener('click', e=>{
+    Bissecao()
+})
+
+function Bissecao(){
+    
+    var a, b, x0, e, fx0, r_estrela
+    // colocar input dps
+    a = 1
+    b = 2
+    e = 0.1
+    
+    
+
+}
